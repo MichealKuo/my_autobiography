e53b001305806c086ee3a29fadc755001d538d0b
Micheal kuo <sxdcfv20002000@gmail.com>
2022-03-10 15:32:24 +0800

    “overflowDone“

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   return (
     <Router>
       <Navbar />
+      <Home />
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
