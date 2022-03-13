aaea72e0884dfdcc3ca7bbe8e4818a025b15dd49
Micheal kuo <sxdcfv20002000@gmail.com>
2022-03-13 22:29:09 +0800

    “RWDAboutMEDone“

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -16,23 +16,23 @@ function AboutMe() {
         <div className="AboutIcon">
           <div className="iconAward">
             <FaAward className="icon-size " />
-            <h6>Experience</h6>
-            <h6 className="textSize"> less 1 years</h6>
+            <p className="textSizeTitle">Experience</p>
+            <p className="textSize"> less 1 years</p>
           </div>
           <div className="iconWorks">
             <MdWorkOutline className="icon-size " />
-            <h6>Projects</h6>
-            <h6 className="textSize"> 2 + </h6>
+            <p className="textSizeTitle">Projects</p>
+            <p className="textSize"> 2 + </p>
           </div>
           <div className="iconPer">
             <BiBody className="icon-size " />
-            <h6>Personality</h6>
-            <h6 className="textSize">Perseverance</h6>
+            <p className="textSizeTitle">Personality</p>
+            <p className="textSize">Perseverance</p>
           </div>
         </div>
         <div className="AboutText">
           <h6>
-            I'm a <span>Front-end-Develope Junior </span>trying to get in{" "}
+            I'm a <span>Front-end-Develope Junior </span>trying to get in
             <span>IT department.</span>IT department. Here , you could know what
             i'm working on and what kind of skills , tools and languages i used
             to set up the website. Hope you would like it!
