2d25a765f0d9c4a7842b317c6cae3533a3e96bd4
Micheal kuo <sxdcfv20002000@gmail.com>
2022-03-13 15:43:30 +0800

    “AboutPartDone“

--- src/Collection/collection.js
+++ src/Collection/collection.js
@@ -3,13 +3,15 @@ export const Collection = [
     id: 1,
     url: "https://img.onl/xziPhC",
     title: "Mao-Planet",
-    describe: "im done with it",
+    describe: "This is website about adapting pets",
+    link: "https://github.com/MichealKuo/Project-MK",
   },
   {
     id: 2,
     url: "https://i.imgur.com/dfMuuk1.jpg",
-    title: "Mao-Planet",
-    describe: "im done with it",
+    title: "My-autobiography",
+    describe: "Topic: Me",
+    link: "https://github.com/MichealKuo/my_autobiography",
   },
   {
     id: 3,
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "../Style/AboutMe.css";
 import Me from "../img/AboutMe.JPG";
+import { FaAward } from "react-icons/fa";
+import { MdWorkOutline } from "react-icons/md";
 function AboutMe() {
   return (
     <div className="About">
@@ -9,7 +11,36 @@ function AboutMe() {
           <img src={Me} alt="" className="a-img" />
         </div>
       </div>
-      <div className="a-right">789</div>
+      <div className="a-right">
+        <div className="AboutIcon">
+          <div className="iconAward">
+            <FaAward />
+            <h6>Experience</h6>
+            <h6> less 1 years</h6>
+          </div>
+          <div className="iconWorks">
+            <MdWorkOutline />
+            <h6>Projects</h6>
+            <h6> 2 + </h6>
+          </div>
+          <div className="iconWorks">
+            <MdWorkOutline />
+            <h6>Projects</h6>
+            <h6> 2 + </h6>
+          </div>
+        </div>
+        <div className="AboutText">
+          <h6>
+            I'm a <span>Front-end-Develope Junior </span>trying to get in{" "}
+            <span>IT department.</span>IT department. Here , you could know what
+            i'm working on and what kind of skills , tools and languages i used
+            to set up the website. Hope you would like it!
+          </h6>
+        </div>
+        <div className="btn-contact">
+          <button className="Talk-btn">Let's Talk</button>
+        </div>
+      </div>
     </div>
   );
 }
